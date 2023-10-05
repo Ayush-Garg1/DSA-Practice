27c30d84d822394d3f378dddb7c8b988576ce2dc
Ayush Garg <ayushgr1128@gmail.com>
2023-10-05 12:21:24 +0530

    Stack Implementation
    Implementation of Stack without using Collection framework

--- stackUsingArray.java
+++ stackUsingArray.java
@@ -8,32 +8,32 @@ class stack{
         int[] st = new int[MAX];
     
     public
-        //Constructor
+    //Constructor
         stack(){
             top = -1;
         }
 
-        //push function to add element at the top of the stack
+    //push function to add element at the top of the stack
         void push(int data){
-            st[++top] = data;   //always increment the top before pushing the element
+            st[++top] = data;       //always increment the top before pushing the element
         }
 
-        //pop function removes top most data from the stack
+    //pop function removes top most data from the stack
         void pop(){
             top--;
         }
 
-        //returns the data at the top of the stack
+    //returns the data at the top of the stack
         int atTop(){
             return st[top];
         }
 
-        //check whether the stack is empty or not
+    //check whether the stack is empty or not
         boolean empty(){
             return top == -1;
         }
 
-        //display the stack from top to bottom
+    //display the stack from top to bottom
         void display(){
             if(top == -1){
                 System.out.println("Stack Empty");
@@ -49,7 +49,7 @@ class stack{
         }
 }
 
-//public class
+//public class or Main class of the java file
 public class stackUsingArray{
     public static void main(String[] args){
         stack s = new stack();
