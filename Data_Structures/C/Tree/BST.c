7c899a45fdfff6dff4749bcdefe62cde062a5998
Ayush Garg <ayushgr1128@gmail.com>
2023-12-16 13:02:12 +0530

    Binary Search tree

---
+++ Data_Structures/C/Tree/BST.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *left, *right;
+};
+typedef struct node node;
+
+node* newNode(int data){
+    node *temp = (node*)malloc(sizeof(node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+int count(node *root){
+    if(root == NULL){
+        return 0;
+    }
+    return 1 + count(root->left) + count(root->right);
+}
+
+node* insert(node *root, int val){
+    if(root == NULL){
+        root = newNode(val);
+        return root;
+    }else if(val < root->data){
+        root->left = insert(root->left, val);
+    }else{
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+node* inorderSuccessor(node *root){
+    while(root->left){
+        root = root->left;
+    }
+    return root;
+}
+
+node* delete(node *root, int val){
+    if(root == NULL){
+        return root;
+    } else if(val < root->data){
+        root->left = delete(root->left, val);
+    } else if(val > root->data){
+        root->right = delete(root->right, val);
+    } else{
+        if(root->left == NULL && root->right == NULL){
+            free(root);
+            root = NULL;
+        }else if(root->left == NULL){
+            node *temp = root;
+            root = root->right;
+            free(temp);
+            temp = NULL;
+        }else if(root->right == NULL){
+            node *temp = root;
+            root = root->left;
+            free(temp);
+            temp = NULL;
+        }else{
+            node *suc = inorderSuccessor(root->right);
+            root->data = suc->data;
+            root->right = delete(root->right, suc->data);
+        }
+    }
+    return root;
+}
+
+void preorder(node *root){
+    if(root == NULL){
+        return;
+    }
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(node *root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    printf("%d ",root->data);
+    inorder(root->right);
+}
+
+void postorder(node *root){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ",root->data);
+}
+
+int main(){
+    node *root = NULL;
+    root = insert(root,5);
+    root = insert(root,2);
+    root = insert(root,1);
+    root = insert(root,3);
+    root = insert(root,7);
+    root = insert(root,6);
+    root = insert(root,8);
+    printf("PREORDER : ");
+    preorder(root);
+    printf("\nINORDER : ");
+    inorder(root);
+    printf("\nPOSTORDER : ");
+    postorder(root);
+    printf("\nCount : %d\n",count(root));
+
+    return 0;
+}
